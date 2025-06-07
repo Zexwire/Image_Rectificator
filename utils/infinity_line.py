9efab326d5d2a48caa4f1b8923ee9a2107e988aa
María <89658202+hidra210@users.noreply.github.com>
2025-06-07 19:18:44 +0200

    Update infinity_line.py

--- utils/infinity_line.py
+++ utils/infinity_line.py
@@ -1,7 +1,6 @@
 from classes.points import Point
 import numpy as np
 
-# TODO: Check el caso en el que dos de las líneas sean paralelas
 def calculate_vanish_points(p1: Point, p2: Point, p3: Point, p4: Point) -> list[Point]:
     """
     Calcula la línea del infinito como la unión de los puntos de fuga de las líneas paralelas
@@ -9,11 +8,11 @@ def calculate_vanish_points(p1: Point, p2: Point, p3: Point, p4: Point) -> list[
     """
     # Calcular puntos de infinito como intersecciones de líneas opuestas
     p = intersection([p1, p2], [p3, p4])  # Intersección de lados opuestos 1-2 y 3-4
+    if p is None:
+        p = Point(1, 0, 0)
     q = intersection([p1, p3], [p2, p4])  # Intersección de lados opuestos 1-3 y 2-4
-
-    # Verificar si alguno de los puntos es None (líneas paralelas)
-    if p is None or q is None:
-        raise ValueError("No se pudo calcular la línea del infinito (algunas líneas son paralelas)")
+    if q is None:
+        q = Point(0, 1, 0)
 
     return [p, q]
 
@@ -49,16 +48,3 @@ def intersection(r1, r2):
     y = (a1 * c2 - a2 * c1) / det
 
     return Point(x, y, 1)
-
-"""
-# TODO: Prueba en proyectiva
-
-    # r1 y r2 son listas de dos puntos cada una, en coordenadas homogéneas
-    l1 = np.cross([r1[0].x, r1[0].y], [r1[1].x, r1[1].y])
-    l2 = np.cross([r2[0].x, r2[0].y], [r2[1].x, r2[1].y])
-    inter = np.cross(l1, l2)
-    # Normaliza si el punto no está en el infinito
-    if inter[2] != 0:
-        inter = inter / inter[2]
-    return Point(inter[0],inter[1],inter[2])  # Devuelve [x, y, w]
-"""
